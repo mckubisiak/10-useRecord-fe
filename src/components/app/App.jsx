3f3418ff81ba952ea627337613377eb83ffbfbc9
Kubi Kubisiak <mckubisiak@gmail.com>
2021-09-23 10:16:24 -0700

    hook written and functioning as expected

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const useRecord = (init) => {
+  const [current, setCurrent] = useState(init);
+  const [colorRecord, setColorRecord] = useState([init]);
+  let [colorIndex, setColorIndex] = useState(0);
+  // TODO: Implement me!!
+  const undo = () => {
+    colorIndex = colorIndex - 1;
+    setColorIndex(colorIndex);
+    setCurrent(colorRecord[colorIndex]);
+  };
+
+  const redo = () => {
+    colorIndex = colorIndex + 1;
+    setColorIndex(colorIndex);
+    setCurrent(colorRecord[colorIndex]);
+  };
+
+  const record = (color) => {
+    colorIndex = colorIndex + 1;
+    colorRecord.splice(colorIndex, 0, color);
+    setColorIndex(colorIndex);
+    setCurrent(colorRecord[colorIndex]);
+    setColorRecord(colorRecord);
+  };
+  return {
+    current,
+    undo,
+    redo,
+    record,
+  };
+};
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  const { current, undo, redo, record } = useRecord('#FF0000');
+
+  return (
+    <>
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
+      <input
+        type="color"
+        value={current}
+        onChange={({ target }) => record(target.value)}
+      />
+      <div
+        style={{ backgroundColor: current, width: '10rem', height: '10rem' }}
+      ></div>
+    </>
+  );
 }
